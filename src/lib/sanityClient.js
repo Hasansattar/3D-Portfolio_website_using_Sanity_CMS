3690cb199cc9a91425d1a7066e650570df71c54e
Hassan Sattar <hasansattar650@gmail.com>
2023-07-14 23:05:26 +0500

    3D portfolio website with sanity CMS

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -3,8 +3,8 @@ import {createClient} from "next-sanity"
 export const client=createClient({
   apiVersion:"2023-07-13",
   dataset:"production",
-  projectId:process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId:"l8qe3ozn",
   useCdn:true,
-  token:process.env.NEXT_PUBLIC_SANITY_ACCESSTOKEN
+  token:"skYJvH7tZaNUzIdpu6wcexlahRzapX7tgEV6T6iagCnc7WhYvvKD2odtuIevseGlMgXZO2ONi4Y3PPvEAjaTY8YzFPWHbCKVIHhlFrW305Z3z1YDD4JEGxjLklNJEDWzhAcalbgVn3F4ndDxGQcaehGmr0myjTqlORJbwgz0xha9ZgvPbVjN"
 })
 
