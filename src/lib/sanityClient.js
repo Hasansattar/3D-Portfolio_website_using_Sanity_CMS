ada8967af26ff293c188623f434f90d62f8b2319
Hassan Sattar <hasansattar650@gmail.com>
2023-07-16 19:25:34 +0500

    update

--- src/lib/sanityClient.js
+++ src/lib/sanityClient.js
@@ -2,9 +2,9 @@ import {createClient} from "next-sanity"
 
 export const client=createClient({
   apiVersion:"2023-07-13",
-  dataset:"production",
-  projectId:"l8qe3ozn",
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "l8qe3ozn",
   useCdn:true,
-  token:"skYJvH7tZaNUzIdpu6wcexlahRzapX7tgEV6T6iagCnc7WhYvvKD2odtuIevseGlMgXZO2ONi4Y3PPvEAjaTY8YzFPWHbCKVIHhlFrW305Z3z1YDD4JEGxjLklNJEDWzhAcalbgVn3F4ndDxGQcaehGmr0myjTqlORJbwgz0xha9ZgvPbVjN"
+  token: process.env.NEXT_PUBLIC_SANITY_ACCESSTOKEN || "skYJvH7tZaNUzIdpu6wcexlahRzapX7tgEV6T6iagCnc7WhYvvKD2odtuIevseGlMgXZO2ONi4Y3PPvEAjaTY8YzFPWHbCKVIHhlFrW305Z3z1YDD4JEGxjLklNJEDWzhAcalbgVn3F4ndDxGQcaehGmr0myjTqlORJbwgz0xha9ZgvPbVjN"
 })
 
