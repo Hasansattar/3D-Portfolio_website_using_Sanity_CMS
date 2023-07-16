f689fcf12096250601d0ac876b0ccc019e831dc5
Hassan Sattar <hasansattar650@gmail.com>
2023-07-16 21:39:40 +0500

    update client

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -4,8 +4,9 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
 import CanvasLoader from "@/components/Loader";
 
+
 const Earth = () => {
-  const earth = useGLTF("/planet/scene.gltf");
+  const earth = useGLTF("https://hasansattar-portfolio1.netlify.app/planet/scene.gltf");
 
   return (
     <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
