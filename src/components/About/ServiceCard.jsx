dec79c379976625a35e2d8db08842150a75f5f19
Hassan Sattar <hasansattar650@gmail.com>
2023-07-16 18:34:13 +0500

    update client

--- src/components/About/ServiceCard.jsx
+++ src/components/About/ServiceCard.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 // react tilt
 import { Tilt } from "react-tilt";
--- src/components/Projects/ProjectItem.jsx
+++ src/components/Projects/ProjectItem.jsx
@@ -1,3 +1,4 @@
+"use client"
 import React from "react";
 
 const ProjectItem = ({ title, active, setSelected, id }) => {
