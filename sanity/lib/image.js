e3d990eb784aaa66f4b1db3a4c3ae78d125074d9
Hassan Sattar <hasansattar650@gmail.com>
2023-07-16 22:29:14 +0500

    update client

--- sanity/lib/image.js
+++
@@ -1,9 +0,0 @@
-import createImageUrlBuilder from '@sanity/image-url'
-import {client} from "@/lib/sanityClient"
-
-
-const imageBuilder = createImageUrlBuilder(client)
-
-export const urlForImage = (source) => {
-  return imageBuilder?.image(source)
-}
